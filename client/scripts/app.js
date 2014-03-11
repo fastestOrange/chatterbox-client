e8d19a4d5d6b8e5451ff5edf07f8a31f50c7eb2c
Hack Reactor Students <coliver303@gmail.com>
2014-03-11 16:12:29 -0700

    working inti fetch refactor

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,129 +1,122 @@
-// YOUR CODE HERE:
+/* global $ , _  */
 
+// YOUR CODE HERE:
+var app = {};
 var userName = 'anonymous';
 
-var App = function(){
-  this.room = '4chan';
-};
 
-App.prototype.init = function(){
-// get an array of rooms
-///make some buttons based on that array
-};
 
-App.prototype.send = function(message){
-  var self = this;
-  $.ajax({
-    // always use this url
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'POST',
-    data: JSON.stringify(message),
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-      self.fetch();
-    },
-    error: function (data) {
-      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
-    }
-  });
-};
+app.init = function(){
+  // get an array of rooms
+  ///make some buttons based on that array
 
-App.prototype.hackFilter = function(data){
-  var washedData = [];
-  for (var i =0; i < data.results.length; i++){
-    var message = data.results[i].text;
-    var username = data.results[i].username;
-    //console.dir(message);
-    if((message!==undefined)&&(username!==undefined)){
-      if(message.match(/^[0-9a-zA-Z]{1,16}$/)&&username.match(/^[0-9a-zA-Z]{1,16}$/)){
-        washedData.push(data.results[i]);
-      }else{
-        console.log(data.results[i]);
+  app.room = 'lobby';
+
+  app.send = function(message){
+    $.ajax({
+      // always use this url
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+        app.fetch();
+      },
+      error: function (data) {
+        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
       }
-    }
-  }
-};
+    });
+  };
 
+  app.fetch = function(callback){
+    $.ajax({
+        url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
+        type: 'GET',
 
-App.prototype.roomFilter = function(data, room){
+        data: where={'results':{'$lte':25}},
+        contentType: 'application/json',
+        success: callback,
+        error: function (data) {
+          console.error('chatterbox: Failed to send message');
+        }
 
-  var currentRoom = room;
-  var result = _.filter(data.results, function(item){
-    return item.roomname===currentRoom;
-  });
-  return result;
+      });
+  };
 
-};
-
-App.prototype.oneFriendFilter = function(data, friend){
-
-};
-
-App.prototype.allFriendsFilter = function(data){};
-
-
-
-$('#post').on('click', function(){
-  var postText = $('#textarea').val();
-  var message = {};
-  message.username = userName;
-  message.text = postText;
-  message.roomname = app.room;
-  console.log(message);
-  app.send(message);
-
-});
-
-$('button .room').on('click', function(){
-  app.fetch(function(databack){
-    var cleanData = app.hackFilter(databack);
-    cleanData = app.roomFilter(cleanData, this.room);
-    app.displayData(cleanData);
-  });
-});
-
-
-App.prototype.filteredDisplay = function(filter, data){
-
+  app.hackFilter = function(data){
+    var washedData = [];
     for (var i =0; i < data.results.length; i++){
       var message = data.results[i].text;
       var username = data.results[i].username;
-      //console.dir(message);
-      if((message!==undefined)&&(username!==undefined)){
-        if(message.match(/^[0-9a-zA-Z]{1,16}$/)&&username.match(/^[0-9a-zA-Z]{1,16}$/)){
-
-        } else {
-          //$('.messages').append('<li>HACK ATTEMPT</li>');
-          console.log(message);
+      var roomName = data.results[i].roomname;
+      if((message!==undefined)&&(username!==undefined)&&(roomName!==undefined)){
+        if(message.match(/^[0-9a-zA-Z]{1,16}$/)&&username.match(/^[0-9a-zA-Z]{1,16}$/)&&roomName.match(/^[0-9a-zA-Z]{1,16}$/)){
+          washedData.push(data.results[i]);
+        }else{
+          console.log(data.results[i]);
         }
       }
     }
   };
 
-App.prototype.display = function(washedData){
-   $('.messages li').remove();
-   for( var i = 0; i < washedData.length; i++){
-     $('.messages').append('<li><a href ="#">'+username+'</a>: '+message+'</li>');
-   }
 
-};
+  app.roomFilter = function(data, room){
 
-
-App.prototype.fetch = function(callback){
-  $.ajax({
-      url: 'https://api.parse.com/1/classes/chatterbox?order=-createdAt',
-      type: 'GET',
-
-      data: where={'results':{'$lte':25}},
-      contentType: 'application/json',
-      success: callback,
-      error: function (data) {
-        console.error('chatterbox: Failed to send message');
-      }
-
+    var currentRoom = room;
+    var result = _.filter(data.results, function(item){
+      return item.roomname===currentRoom;
     });
+    return result;
+
+  };
+
+  app.oneFriendFilter = function(data, friend){
+    var currentFriend = friend;
+    var result = _.filter(data.results, function(item){
+      return item.roomname === currentFriend;
+    });
+    return result;
+
+  };
+
+  app.allFriendsFilter = function(data){
+    var result = _.filter(data.results, function(item){
+      // if the item.username === a key in user.friends
+      return item.username === (friend in user.friends);
+    });
+    return result;
+  };
+
+
+
+  $('#post').on('click', function(){
+    var postText = $('#textarea').val();
+    var message = {};
+    message.username = userName;
+    message.text = postText;
+    message.roomname = app.room;
+    console.log(message);
+    app.send(message);
+  });
+
+  $('button .room').on('click', function(){
+    app.fetch(function(databack){
+      var cleanData = app.hackFilter(databack);
+      cleanData = app.roomFilter(cleanData, app.room);
+      app.displayData(cleanData);
+    });
+  });
+
+  app.display = function(washedData){
+    $('.messages li').remove();
+    for( var i = 0; i < washedData.results.length; i++){
+    $('.messages').append('<span><a href ="#">'+washedData.results[i].username+'</a>: '+washedData.results[i].text+'</span>');
+    ///apend roomnames to roomlist
+    }
+  };
+
 };
 
 var User = function(username){
@@ -147,7 +140,8 @@ User.prototype.addFriend = function(username){
 //forFriend will display all friends
 
 
-var app = new App();
+//var app = new App();
+
 // call init
 // setInterval(app.fetch, 3000);
 
